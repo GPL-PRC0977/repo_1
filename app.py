56ee736baf2f1013e938c6b461cf23bc7c87d1cc
Gilbert Laman <gilbert.laman@primergrp.com>
2026-08-06 10:27:39 +0800

    Update app.py for new feature

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
 print("This is a sample Python application.")
 print("------------------------------------------------")
 print("Hello, World!")
+print("This is a new feature branch for testing purposes.")
